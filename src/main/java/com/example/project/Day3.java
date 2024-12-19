ca41b0e13cd7524fe875df12bbd1764a414ce8e6
Diyor52 <diyora2@nycstudents.net>
2024-12-19 17:00:25 +0000

    Diyor and Randy challenge

--- src/main/java/com/example/project/Day3.java
+++ src/main/java/com/example/project/Day3.java
@@ -1,20 +1,36 @@
-package com.example.project;
+// package com.example.project;
 
-public class Day3 {
-      public static String[][] generateSnowflake(int size) { //you will be tested on this method
-        String[][] grid = new String[size][size];
-
-    
-
-        return grid;
-    }
-
-    // Prints the snowflake  will be useful if tests fail (you will not be tested on this method)
-    public static void printSnowflake(String[][] snowflake) {
+// public class Day3 {
+//     public static String[][] generateSnowflake(int size) { 
+//         String[][] grid = new String[size][size];
         
-    }
+//         // creates grid for the snowflake. Creates rows and columns equal to the size.
+//         for (int i = 0; i < size; i++) { 
+//             for (int k = 0; k < size; k++) {
+//                 grid[i][k] = " ";
+//             }
+//         }
 
-    // Test for the snowflake generation
-    public static void main(String[] args) {
-    }
-}
+//         // inserts asterisks into the grid. Detects if size is an odd number, and gets the center. Then puts the asterisks at the center the on the ouside.
+//         if (size % 2 == 1) {
+//             int center = size / 2;
+//             for (int i = 0; i < size; i++) {
+//                 grid[i][center] = "*";
+//                 grid[center][i] = "*";
+//                 grid[i][i] = "*";
+//                 grid[i][size - i - 1] = "*";
+//             }
+//         }
+
+//         return grid;
+//     }
+
+//     // Prints the snowflake  will be useful if tests fail (you will not be tested on this method)
+//     public static void printSnowflake(String[][] snowflake) {
+        
+//     }
+
+//     // Test for the snowflake generation
+//     public static void main(String[] args) {
+//     }
+// }
--- src/main/java/com/example/project/Day4.java
+++ src/main/java/com/example/project/Day4.java
@@ -6,34 +6,62 @@ public class Day4 {
     private static String[] reindeer_names = {"Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"};
 
     public static class Reindeer { // you must have at least a name, distance travelled, speed, run duration, and rest duration attribute
-
+        private String name;
+        private int distanceTraveled;
+        private int speed;
+        private int runDuration;
+        private int restDuration;
 
         // Constructor 
         public Reindeer(String name, int speed, int runDuration, int restDuration) {
+            this.name = name;
+            this.speed = speed;
+            this.runDuration = runDuration;
+            this.restDuration = restDuration;
+            distanceTraveled = 0;
         }
 
         public int getDistanceTraveled(){ // this method is required
-            return 0;
+            return distanceTraveled;
         }
         
 
         public void simulateSecond(){ //this method is required but not tested
-            
+            if (runDuration > 0) {
+                runDuration --;
+                distanceTraveled += speed;
+            }
+            else {
+                restDuration --;
+            }
+        }
+
+        public String getName() {
+            return name;
         }
 
     }
 
     public static String simulateRace(int time, Reindeer[] reindeers){ //you will be tested on this method
-        return "name of winner";
+        int maxDistance = 0;
+        String winner = null;
+
+        for (int i = 0; i < reindeers.length; i++) {
+            for (int k = 0; k< time; k++) {
+                reindeers[i].simulateSecond();
+            }
+            if (reindeers[i].getDistanceTraveled() > maxDistance) {
+                maxDistance = reindeers[i].getDistanceTraveled();
+                winner = reindeers[i].getName();
+            }
+        }
+        return winner;
     }
 
     
     public static void main(String[] args) { // for testing purposed
-
+       
     }
-
- 
 }
-
 
 
